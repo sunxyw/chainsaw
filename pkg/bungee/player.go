0444c386f5fa93091b17b369451ffe7b602d7018
sunxyw <31698606+sunxyw@users.noreply.github.com>
2022-07-16 09:15:06 +0000

    supress bungee player ip in json response

--- pkg/bungee/player.go
+++ pkg/bungee/player.go
@@ -3,6 +3,6 @@ package bungee
 type BungeePlayer struct {
 	UUID   string `json:"uuid"`
 	Name   string `json:"name"`
-	IP     string `json:"ip"`
+	IP     string `json:"-"`
 	Server string `json:"server"`
 }
